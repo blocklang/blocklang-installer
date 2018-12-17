ec2510e7ba0ca76be322727445c49c531772cb20
xiaohulu <idocument@qq.com>
2018-12-17 15:24:03 +0800

    创建 Cli 项目

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-//! 在此处写需求
-//! 
+use structopt::StructOpt;
 
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    match args {
+        Cli::Update => {
+            println!("{}", "安装成功");
+        }
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "installer", about = "Block Lang 安装程序")]
+enum Cli {
+    /// 从软件中心更新软件，并安装到应用服务器上。
+    #[structopt(name = "update")]
+    Update,
 }
