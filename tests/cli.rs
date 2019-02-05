28b191d830f00bc4b727f4c15751752072126f56
xiaohulu <idocument@qq.com>
2019-02-05 17:52:14 +0800

    将所有依赖升级到最新版本，并调整不兼容的代码。

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,10 +1,11 @@
 use std::process::Command;
-use assert_cmd::prelude::{CommandCargoExt};
+use assert_cmd::prelude::*;
 
 #[test]
 fn command_update_success() -> Result<(), Box<std::error::Error>> {
-    let mut cmd = Command::main_binary()?;
+    let mut cmd = Command::cargo_bin("installer")?;
     cmd.arg("update");
+    cmd.assert().success();
     // cmd.assert().failure()
     //     .stdout(predicate::str::contains("更新失败"));
     // TODO: 考虑如何准备好前置条件，以做详细的集成测试。
